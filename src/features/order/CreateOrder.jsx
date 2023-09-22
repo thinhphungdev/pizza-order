a7c26a584131e107e1896c5941f18900bd7eecdf
howardphungdev <phungthinh0946519356@gmail.com>
2023-09-22 18:11:28 +0700

    feat: integrate geolocation api to fetch user address and position

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -16,7 +16,15 @@ const isValidPhone = (str) =>
   );
 
 function CreateOrder() {
-  const username = useSelector((state) => state.user.username);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === "loading";
+
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
   const dispatch = useDispatch();
@@ -34,8 +42,6 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
-
-      <Button onClick={() => dispatch(fetchAddress())}>Get Position</Button>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
@@ -62,16 +68,38 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               className="input w-full"
               type="text"
               name="address"
+              defaultValue={address}
+              disabled={isLoadingAddress}
               required
             />
+
+            {addressStatus === "error" && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
+          <span className="absolute right-[3px] top-[3px] z-10 md:right-[5px] md:top-[5px]">
+            {!position.latitude && !position.longitude && (
+              <Button
+                type="small"
+                disabled={isLoadingAddress || isSubmitting}
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                Get Position
+              </Button>
+            )}
+          </span>
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -90,6 +118,15 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.latitude && position.longitude
+                ? `${position.latitude}, ${position.longitude}`
+                : ""
+            }
+          />
           <Button disabled={isSubmitting} type="primary">
             {isSubmitting
               ? "Placing order...."
@@ -116,6 +153,8 @@ export async function action({ request }) {
       "Please give us your correct phone number. We might need it to contact you.";
 
   if (Object.keys(errors).length > 0) return errors;
+
+  console.log(order, "dc");
 
   // If everything is okay, create new order and redirect
 
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -3,7 +3,7 @@ import { getAddress } from "../../services/apiGeocoding";
 
 function getPosition() {
   return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
+    navigator?.geolocation?.getCurrentPosition(resolve, reject);
   });
 }
 
@@ -45,10 +45,9 @@ const userSlice = createSlice({
   },
   extraReducers: (builder) =>
     builder
-      .addCase(
-        fetchAddress.pending,
-        (state, action) => (state.status = "loading"),
-      )
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = "loading";
+      })
       .addCase(fetchAddress.fulfilled, (state, action) => {
         state.status = "idle";
         state.position = action.payload.position;
@@ -56,6 +55,8 @@ const userSlice = createSlice({
       })
       .addCase(fetchAddress.rejected, (state, action) => {
         state.status = "error";
+        // state.error =
+        //   "There was a problem getting your address. Make sure to fill this field";
         state.error = action.error.message;
       }),
 });
